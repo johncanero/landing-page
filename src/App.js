842f7e4a20a6c4221f5f25150fb7034d6ec24e71
John Canero <johncanero.official@gmail.com>
2022-08-29 18:04:17 +0800

    Include Screen and Config Requirements, Update Spacing, Add Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 
 function App() {
   return (
     <div>
-         <h1 className="text-3xl font-bold underline">
-             Hello world!
-        </h1>
+        <Main />
+        <Navbar />
     </div>
   );
 }
