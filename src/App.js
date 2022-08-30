b118835209a71501ebc581288f8d380b3f0efe54
John Canero <johncanero.official@gmail.com>
2022-08-30 23:18:45 +0800

    Call To Action Section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Feature from './components/Feature';
 import Testimonials from './components/Testimonials';
+import CallToAction from './components/CallToAction';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <Feature />
         <Testimonials />
+        <CallToAction />
     </div>
   );
 }
