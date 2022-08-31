a29be5daeef672e932f7397c9801ee47207d6701
John Canero <johncanero.official@gmail.com>
2022-08-31 16:38:33 +0800

    Spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,7 +40,6 @@ const Navbar = () => {
                 <button 
                     id='menu-btn' 
                     className='block hamburger md:hidden focus:outline-none'>
-
                     <span className='hamburger-top'></span>
                     <span className='hamburger-middle'></span>
                     <span className='hamburger-bottom'></span>
