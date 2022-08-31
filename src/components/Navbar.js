253548b54478b4bada2bdb3beaa0c93a1a756a90
John Canero <johncanero.official@gmail.com>
2022-08-31 17:34:49 +0800

    Comment JS for the Toggle Button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import logo from '../images/logo.svg'
 
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+// const btn = document.getElementById('menu-btn')
+// const nav = document.getElementById('menu')
 
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open')
-  nav.classList.toggle('flex')
-  nav.classList.toggle('hidden')
-})
+// btn.addEventListener('click', () => {
+//   btn.classList.toggle('open')
+//   nav.classList.toggle('flex')
+//   nav.classList.toggle('hidden')
+// })
 
 const Navbar = () => {     
   return (
