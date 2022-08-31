b494a7816ec4cf7877533ca313c06e1cf2e7ca98
John Canero <johncanero.official@gmail.com>
2022-08-31 15:51:40 +0800

    Background Images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,6 +26,27 @@ const Navbar = () => {
                 <div>
                     <a href='#' className='hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block'>Get Started</a>
                 </div>
+
+                {/* Hambuger Menu */}
+                <button 
+                    id='menu-btn' 
+                    lassName='block hamburger md:hidden focus:outline-none'>
+
+                    <span className='hamburger-top'></span>
+                    <span className='hamburger-middle'></span>
+                    <span className='hamburger-bottom'></span>
+                </button>
+            </div>
+
+            {/* Mobile Menu */}
+            <div className='md:hidden'>
+                <div id='menu' className='absolute flex flex-col items-center hidden self-end  py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md'>
+                    <a href='#'>Pricing</a>
+                    <a href='#'>Product</a>
+                    <a href='#'>About</a>
+                    <a href='#'>Careers</a>
+                    <a href='#'>Community</a>
+                </div>
             </div>
         </nav>
     </div>
