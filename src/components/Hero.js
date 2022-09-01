0d7ad790db1ed6c666bf872f82a5e59141a8794c
John Canero <johncanero.official@gmail.com>
2022-09-01 08:58:15 +0800

    Adjust Code

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div>
        <section id='hero'>
            {/* Flex Container */}
-           <div className='container flex flex-col-reverse md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0'>
+           <div className='container flex flex-col-reverse items-center md:flex-row px-6 mx-auto mt-10 space-y-0 md:space-y-0'>
            
                 {/* Left Item */}
                 <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
